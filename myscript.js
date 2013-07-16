404f9762ac5f97288e096c1566b483dfa32d4104
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2013-07-16 13:26:19 +0900

    完成

---
+++ myscript.js
@@ -0,0 +1,14 @@
+alert('集中してください');
+
+
+
+var randnum = Math.floor( Math.random() * 4 );
+if (randnum == 1) {
+	location.href="http://www.youtube.com/watch?v=KTXwhAg1p50#start=0:06;";
+}else if(randnum == 2){
+	location.href="http://www.youtube.com/watch?v=UpCyVX9EVWY#start=0:06;";
+}else if(randnum == 3){
+	location.href="http://www.youtube.com/watch?v=5-4RxKWjPnI#start=0:06;";
+}else{
+	location.href="http://www.youtube.com/watch?v=1-34zLFf25g#start=0:06;";
+};
